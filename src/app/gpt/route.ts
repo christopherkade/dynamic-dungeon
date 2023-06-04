2ef7b4907b09aa80abc687b9d9f3770681b6e95a
Christopher Kade <christopher.kade@openclassrooms.com>
2023-06-04 19:08:27 +0300

    Testing

--- src/app/gpt/route.ts
+++ src/app/gpt/route.ts
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import { ChatGPTAPI } from "chatgpt";
 
-export async function GET() {
-  return NextResponse.json({ data: { text: "hello world!" } });
+export async function GET(request: Request) {
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY || "", // Your OpenAI API Key
+  });
+  const openAiRes = await api.sendMessage(
+    request.body.message || "hello world!"
+  );
+
+  return NextResponse.json({ data: openAiRes.text });
 }
